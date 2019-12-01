26355dd690348a237d127b2600d6c788882b1ba7
jwbubujw <303438914@qq.com>
2019-12-01 21:50:09 +0800

    增加接口调试

--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -1,13 +1,8 @@
+const db = wx.cloud.database();
 Page({
   data: {
-    markers: [{
-      iconPath: "/images/movie_select.png",
-      id: 0,
-      latitude: 34.350369,
-      longitude: 108.94474,
-      width: 50,
-      height: 50
-    }],
+    reslut: [],
+    markers: [],
     polyline: [{
       points: [{
         longitude: 113.3245211,
@@ -19,27 +14,66 @@ Page({
       color: "#FF0000DD",
       width: 12,
       dottedLine: true
-    }],
-    controls: [{
-      id: 1,
-      iconPath: '/images/movie_select.png',
-      position: {
-        left: 0,
-        top: 300 - 50,
-        width: 50,
-        height: 50
+    }]
+  },
+  // regionchange(e) {
+  //   console.log(e.type)
+  // },
+  // markertap(e) {
+  //   console.log(e.markerId)
+  // },
+  // controltap(e) {
+  //   console.log(e.controlId)
+  // },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function() {
+    //保存系统信息
+    var _this = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        _this.setData({
+          view: {
+            Height: res.windowHeight
+          }
+        })
       },
-      clickable: true
-    }],
-
-  },
-  regionchange(e) {
-    console.log(e.type)
-  },
-  markertap(e) {
-    console.log(e.markerId)
-  },
-  controltap(e) {
-    console.log(e.controlId)
+    });
+     //请求测站信息
+    wx.cloud.callFunction({
+      name: 'movielist'
+    }).then(res => {
+      let a = JSON.parse(res.result).result
+      for (let i = 0; i < a.length; i++) {
+        _this.setData({
+          markers: _this.data.markers.concat({
+            iconPath: "/images/red.png",
+            id: i,
+            latitude: parseFloat(a[i].lttd),
+            longitude: parseFloat(a[i].lgtd),
+            width: 12,
+            height: 12
+          })
+        })
+      }
+    }).catch(err => {
+      console.error(err);
+    });
+    wx.cloud.callFunction({
+      name: 'geolist'
+    }).then(res => {
+      let a = JSON.parse(res.result).result
+      for (let i = 0; i < a.length; i++) {
+        db.collection('bound').add({
+          data: {
+            geo: new db.Geo.Point(110, 23)
+          }
+        })
+      }
+    }).catch(err => {
+      console.error(err);
+    });
+   
   }
 })

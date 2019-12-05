993461cc249bda2a5bc46dfe26f266d0906ea05c
jwbubujw <303438914@qq.com>
2019-12-05 22:30:06 +0800

    增加边界

--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -1,20 +1,9 @@
 const db = wx.cloud.database();
 Page({
   data: {
-    reslut: [],
+    points: [],
     markers: [],
-    polyline: [{
-      points: [{
-        longitude: 113.3245211,
-        latitude: 23.10229
-      }, {
-        longitude: 113.324520,
-        latitude: 23.21229
-      }],
-      color: "#FF0000DD",
-      width: 12,
-      dottedLine: true
-    }]
+    polyline: []
   },
   // regionchange(e) {
   //   console.log(e.type)
@@ -28,11 +17,11 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function() {
+  onLoad: function () {
     //保存系统信息
     var _this = this;
     wx.getSystemInfo({
-      success: function(res) {
+      success: function (res) {
         _this.setData({
           view: {
             Height: res.windowHeight
@@ -40,40 +29,68 @@ Page({
         })
       },
     });
-     //请求测站信息
-    wx.cloud.callFunction({
-      name: 'movielist'
-    }).then(res => {
-      let a = JSON.parse(res.result).result
-      for (let i = 0; i < a.length; i++) {
-        _this.setData({
-          markers: _this.data.markers.concat({
-            iconPath: "/images/red.png",
-            id: i,
-            latitude: parseFloat(a[i].lttd),
-            longitude: parseFloat(a[i].lgtd),
-            width: 12,
-            height: 12
-          })
-        })
-      }
-    }).catch(err => {
-      console.error(err);
-    });
-    wx.cloud.callFunction({
-      name: 'geolist'
-    }).then(res => {
-      let a = JSON.parse(res.result).result
-      for (let i = 0; i < a.length; i++) {
-        db.collection('bound').add({
-          data: {
-            geo: new db.Geo.Point(110, 23)
+
+    var total = 0;
+    db.collection('bound').count({
+      success: function (res) {
+        total = res.total;
+        const batchTimes = Math.ceil(total / 20);
+        for (let i = 0; i < batchTimes; i++) {
+          db.collection('bound').orderBy('id', 'asc').skip(i * 20).get().then(res => {
+            for (let j = 0; j < res.data.length; j++) {
+              console.log(res.data[j].smx)
+              _this.setData({
+                points: _this.data.points.concat({
+                  longitude: parseFloat(res.data[j].smx),
+                  latitude: parseFloat(res.data[j].smy)
+                })
+              })
+              if (i == batchTimes - 1) {
+                _this.setData({
+                  polyline: [{
+                    points: _this.data.points,
+                    color: "#FF0000DD",
+                    width: 1,
+                    dottedLine: false
+                  }]
+                })
+              }
+            }
           }
-        })
+          ).catch(err => {
+
+          });
+        }
       }
-    }).catch(err => {
-      console.error(err);
-    });
-   
+    })
+
+
+
+
+    //请求测站信息
+    // wx.showLoading({
+    //   title: '正在加载数据',
+    // })
+    // wx.cloud.callFunction({
+    //   name: 'movielist',
+    // }).then(res => {
+    //   wx.hideLoading();
+    //   let a = JSON.parse(res.result).result
+    //   for (let i = 0; i < a.length; i++) {
+    //     _this.setData({
+    //       markers: _this.data.markers.concat({
+    //         iconPath: "/images/red.png",
+    //         id: i,
+    //         latitude: parseFloat(a[i].lttd),
+    //         longitude: parseFloat(a[i].lgtd),
+    //         width: 12,
+    //         height: 12
+    //       })
+    //     })
+    //   }
+    // }).catch(err => {
+    //   console.error(err);
+    // });
+
   }
 })
